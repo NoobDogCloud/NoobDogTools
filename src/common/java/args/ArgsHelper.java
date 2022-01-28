ece3747c74856ca91e20a32c2b84ba842f2e04e7
putao520 <yuyao1022@hotmail.com>
2022-01-28 13:46:34 +0800

    增加参数转换支持

---
+++ src/common/java/args/ArgsHelper.java
@@ -0,0 +1,23 @@
+package common.java.args;
+
+import java.util.HashMap;
+import java.util.Set;
+
+public class ArgsHelper {
+    public static HashMap<String, Object> dictionary(String[] args) {
+        HashMap<String, Object> dictionary = new HashMap<>();
+        for (int i = 0; i < args.length; i++) {
+            String arg = args[i];
+            if (arg.startsWith("-")) {
+                String key = arg.substring(1);
+                Object value = args[i + 1];
+                dictionary.put(key, value);
+            }
+        }
+        return dictionary;
+    }
+
+    public static Set<String> keys(String[] args) {
+        return dictionary(args).keySet();
+    }
+}
